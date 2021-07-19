4f4bfb8b8e0179cfe5c1960836edd25296b85d9b
Diwei Guan <diwei89@hotmail.com>
2021-07-19 19:10:39 -0400

    Updated how it searches for the movie title, so that the user does not have to search for the whole title and year.

--- recommender.py
+++ recommender.py
@@ -53,20 +53,29 @@ cos_sim = cosine_similarity(matrix, matrix)
 #creates a mapping of each title to an index
 mapping = pd.Series(movies.index, index=movies['title'])
 
+def select_movie(input):
+    #grabs the word closes to the input (makes search more flexible)
+    mov = movies[movies['title'].str.contains(input)]
+    #grabs just the title of the movie
+    mov = mov['title'].values[0]
+    return mov
+
 
 def movie_recommender(input):
+    mov_input = select_movie(input)
+    print("Recommend a movie similar to: ", mov_input)
     #finds the movie based on input
-    movie = mapping[input]
+    movie = mapping[mov_input]
     #gets the similarity score of the movie
     sim_score = list(enumerate(cos_sim[movie]))
     #grabs the most similar movies by reversing the list
     sim_score = sorted(sim_score, key=lambda x: x[1], reverse=True)
     #grabs the first 10 movies
     sim_score = sim_score[1:11]
-    #gets the movie ids of the top 10 movies
+    #gets the movie ids of the top 10 movies+
     indices = [i[0] for i in sim_score]
     #returns the movies with their IDs and title in a list
     return movies['title'].iloc[indices]
 
-#print(movie_recommender("Avengers: Infinity War - Part I (2018)"))
+print(movie_recommender("Avengers"))
 
